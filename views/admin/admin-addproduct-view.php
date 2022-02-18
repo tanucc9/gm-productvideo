51f73dd664896e80e85814a6d98bfcc1d5b76980
tanucc9 <maurogaetano997@gmail.com>
2022-02-18 16:57:50 +0100

    Added styles to add product page and multiselect

--- views/admin/admin-addproduct-view.php
+++ views/admin/admin-addproduct-view.php
@@ -12,12 +12,17 @@ $options= ["Cl. 16", "Cl. 13", "Cl. 21", "Cl. 25", "Cl. 30"];
 
 <div class="wrapper-addproduct-form">
     <form action="<?php echo admin_url('admin.php?page=' . PARENT_SLUG_ADMIN_TAB . '-pv-add-productvideo', 'https'); ?>" method="post">
-        <div class="row" id="first-row">
-            <div class="col-sm-3">
+        <div id="first-row-title-url">
+            <div class="gm_pv_wrap_input gm_pv_wrap_input_title_prod">
                 <input type="text" placeholder="Name Product*" name="name_product" required>
             </div>
-            <div class="col-sm-2" id="select-category-col">
-                <select name="category_select" id="category_select">
+            <div class="gm_pv_wrap_input gm_pv_wrap_input_url">
+                <input type="text" placeholder="URL Video*" name="url_video" required>
+            </div>
+        </div>
+        <div class="second-row-multiselect">
+            <div class="gm_pv_wrap_multiselect">
+                <select name="category_select" id="category_select" multiple>
                     <option value="0">Choose category</option>
                     <?php foreach ($options as $key => $option) { ?>
                         <option value="<?php echo $key + 1 ?>"><?php echo $option ?></option>
@@ -25,17 +30,8 @@ $options= ["Cl. 16", "Cl. 13", "Cl. 21", "Cl. 25", "Cl. 30"];
                 </select>
             </div>
         </div>
-        <div class="row">
-            <div class="col-sm-5">
-                <input type="text" placeholder="URL Video*" name="url_video" required>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-5" id="col-submit-form">
-                <button type="submit" class="btn btn-primary" id="submit-addproduct-form">Save</button>
-            </div>
+        <div class="gm_pv_wrap_submit">
+            <button type="submit" class="button" id="submit-addproduct-form">Save</button>
         </div>
         <input type="hidden" name="action" value="add_product">
     </form>
-</div>
-</div>
