d496b2b50b69467aabd728fa47487dc0008c8e0a
tanucc9 <maurogaetano997@gmail.com>
2022-02-23 15:13:59 +0100

    lazy load for videos

---
+++ assets/js/front/lazy-load-video-yt.js
@@ -0,0 +1,41 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+let currentIconStartPlayer;
+
+jQuery(".gm_pv_preview_video").on("click", function() {
+
+   if (currentIconStartPlayer) {
+      currentIconStartPlayer.show();
+   }
+   removeActualVideo();
+
+   const embedCode = jQuery(this).attr("data-embed-code");
+   const containerVideo = jQuery(this).parent().attr('id');
+
+   player = new YT.Player(containerVideo, {
+      height: '100%',
+      width: '100%',
+      videoId: embedCode,
+      events: {
+         'onReady': onPlayerReady,
+      }
+   });
+
+   currentIconStartPlayer = jQuery(this);
+   jQuery(this).hide();
+});
+
+function onPlayerReady(event) {
+   event.target.playVideo();
+}
+
+function removeActualVideo() {
+   if (player) {
+      player.destroy();
+   }
+}
--- views/front/front-list-products-view.php
+++ views/front/front-list-products-view.php
@@ -4,12 +4,19 @@
     </div>
     <div class="gm_pv_container_products">
         <?php if(isset($products) && count($products) > 0) {
-            foreach ($products as $product) { ?>
-                <div class="gm_pv_product" id="product_<?php echo $product->id ?>">
-                    <div class="gm_pv_container_video">
-                        <iframe width="420" height="315"
-                                src="https://www.youtube.com/embed/3chV1zYSj24">
-                        </iframe>
+            foreach ($products as $product) {
+                $embedCodeYT = explode("https://www.youtube.com/embed/", $product->url_video)[1];
+            ?>
+                <div class="gm_pv_product" id="product_<?php echo $product->id ?>" data-id-product="<?php echo $product->id ?>">
+                    <div class="gm_pv_container_video" id="gm_pv_container_video_<?php echo $product->id ?>">
+                        <div class="gm_pv_preview_video" data-embed-code="<?php echo $embedCodeYT ?>">
+                            <img src="<?php echo get_site_url() ?>/wp-content/plugins/gm-productvideo/assets/img/player-start.png" />
+                        </div>
+                        <style>
+                            .gm_pv_container_video {
+                                background: url(https://img.youtube.com/vi/<?php echo $embedCodeYT ?>/sddefault.jpg);
+                            }
+                        </style>
                     </div>
                     <div class="gm_pv_container_info">
                         <div class="gm_pv_container_title">
@@ -25,6 +32,7 @@
 </div>
 
 <link rel="stylesheet" type="text/css" href="<?php echo get_site_url(null, 'wp-content/plugins/gm-productvideo/assets/css/front/list-products.css') ?>">
+<script type="application/javascript" src="<?php echo get_site_url(null, 'wp-content/plugins/gm-productvideo/assets/js/front/lazy-load-video-yt.js') ?>"></script>
 
 <?php
 include(GM_PV__PLUGIN_DIR . 'views/parts/pagination.php');
