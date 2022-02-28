e6c82edffec5974ddb216630d373610fe901e767
tanucc9 <maurogaetano997@gmail.com>
2022-02-28 15:47:22 +0100

    Classes not used anymore removed

--- views/admin/admin-list-view.php
+++ views/admin/admin-list-view.php
@@ -3,7 +3,6 @@
 <?php
     include(GM_PV__PLUGIN_DIR . 'views/parts/admin-header.php');
     include(GM_PV__PLUGIN_DIR . 'views/parts/alert.php');
-    include(GM_PV__PLUGIN_DIR . 'views/parts/alert_ajax.php');
 ?>
 
     <form id="events-filter" method="get">
